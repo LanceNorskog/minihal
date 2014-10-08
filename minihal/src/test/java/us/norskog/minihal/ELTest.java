0c274544056bef2402f92762b6c411bd7206ecd8
Lance Norskog <goksron@yahoo.com>
2014-10-07 17:58:23 -0700

    Diff won't tell the changes?

--- minihal/src/test/java/us/norskog/minihal/ELTest.java
+++ minihal/src/test/java/us/norskog/minihal/ELTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+
 public class ELTest {
     Request request;
     Response response;
@@ -15,8 +17,7 @@ public class ELTest {
     public void setUp() throws Exception {
         request = new Request();
         response = new Response();
-        item = new Item();
-
+        item = new Item("");
         base = new ELBase();
     }
 
@@ -26,22 +27,68 @@ public class ELTest {
     }
 
     @Test
-    public void checkEL() {
+    public void requests() {
         base.setTypes(request, response, item);
         base.setVars(request, response, item);
-        String abc = base.eval("${request.getAbc()}");
-        Assert.assertEquals("abc", abc);
+        String abc = base.evalExpr("${abc}");
+        Assert.assertEquals("null", abc);
+        abc = base.evalExpr("${request.params.q}");
+        Assert.assertEquals("monkeys", abc);
+        abc = base.evalExpr("${request.params.rows}");
+        Assert.assertEquals("10", abc);
+    }
+
+    @Test
+    public void items() {
+        base.setTypes(request, response, item);
+        base.setVars(request, response, item);
+        Collection<Object> items = base.getItems("${response.itemList}");
+        Assert.assertNotNull(items);
+        Iterator<Object> iter = items.iterator();
+        Assert.assertEquals("A1", iter.next().toString());
+        Assert.assertEquals("A1", iter.next().toString());
+        Assert.assertFalse(iter.hasNext());
+        items = base.getItems("${response.itemArray");
+        Assert.assertNotNull(items);
+        iter = items.iterator();
+        Assert.assertEquals("B1", iter.next().toString());
+        Assert.assertEquals("B2", iter.next().toString());
+        Assert.assertFalse(iter.hasNext());
     }
 }
 
 class Request {
-    public String getAbc() { return "abc";}
+    public Map<String,String> getParams() {
+        Map<String,String> params = new HashMap<String, String>();
+        params.put("q", "monkeys");
+        params.put("rows", "10");
+        return params;
+    }
 }
 
 class Response {
 
+    public String getQ() {return "monkeys";}
+    public String getRows() {return "10";}
+    public Item[] getItemArray() {
+        Item[] items = {new Item("A1"), new Item("A2")};
+        return items;
+    }
+    public List<Item> getItemList() {
+        List<Item> list = new ArrayList<Item>();
+        list.add(new Item("B1"));
+        list.add(new Item("B2"));
+        return list;
+    }
 }
 
 class Item {
+    private final String value;
+    public Item(String value) {
+        this.value = value;
+    }
 
+    public String getValue() {
+        return value;
+    }
 }
